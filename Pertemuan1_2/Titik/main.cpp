dd3033c955f827aa7b3c7a50cfff8313b016f0ea
Maroz26 <ammarrozan05@gmail.com>
2026-03-23 20:01:30 +0700

    Rename folder pertemuan1 ke pertemuan1_2

--- Pertemuan1_2/Titik/main.cpp
+++ Pertemuan1_2/Titik/main.cpp
@@ -4,7 +4,7 @@
 
 void Point(void){
 	glClear(GL_COLOR_BUFFER_BIT);
-	glPointSize(1.0f);
+	glPointSize(10.0f);
 	
 	glBegin(GL_POINTS);
 	glColor3f(1.0f, 0.0f, 0.0f);
@@ -17,7 +17,7 @@ void Point(void){
 
 int main(int argc, char*argv[]) {
 	glutInit(&argc, argv);
-	glutInitWindowsSize(640, 480);
+	glutInitWindowSize(640, 480);
 	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGBA);
 	glutCreateWindow("biru");
 	glutDisplayFunc(Point);
